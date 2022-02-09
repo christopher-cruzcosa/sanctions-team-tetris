196a82b53c1b57c992da35e005c47789fe644a0b
Chris C <christopher.cruzcosa@gmail.com>
2022-02-08 19:15:44 -0800

    change react app

--- client/src/App.js
+++ client/src/App.js
@@ -5,4 +5,5 @@ import Tetris from "./components/Tetris";
 
 export default function App() {
   return <Tetris />;
+  
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.css";
-
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
